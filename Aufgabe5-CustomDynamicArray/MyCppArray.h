3c900139fae8b140c7103a317dcd9d52df29745e
AdrianaLucena <adrianalucena2002@gmail.com>
2024-03-02 01:30:11 +0100

    copied and pasted code for aufgabe 5.

---
+++ Aufgabe5-CustomDynamicArray/MyCppArray.h
@@ -0,0 +1,19 @@
+//
+// Created by adria on 23.02.2024.
+//
+
+#pragma once
+#include "iostream"
+
+class MyCppArray {
+public:
+    int sizeOfArray();
+    int smallestNumber();
+    int biggestNumber();
+
+private:
+    int arr[100];
+
+};
+
+
